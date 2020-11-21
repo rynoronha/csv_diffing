385a0d3864646d6b6c34bbe7a24361ed2ea0ee77
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-11-21 11:55:02 -0300

    implemented get_job endpoint, returning diffed csv

--- diffing/apis/api_diffing.py
+++ diffing/apis/api_diffing.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from diffing.models import Job
 from diffing.tasks import run_diff
 from celery.result import AsyncResult
+import os
 
 
 def new_job(request, metric_id):
@@ -16,7 +17,17 @@ def get_job(request, job_id):
     try:
         job = Job.objects.get(pk=int(job_id))
         if job.ready:
-            return JsonResponse({"job_id": job_id, "status": "ready!"})
+            dirname = os.path.dirname(__file__)
+            file_path = os.path.join(
+                dirname, f'../diffed_files/{job.file_name}',)
+
+            with open(file_path, 'r') as f:
+                file_data = f.read()
+
+            response = HttpResponse(file_data, content_type='text/csv')
+            response['Content-Disposition'] = f'attachment; filename="{job.metric}-diff.csv"'
+
+            return response
         else:
             return JsonResponse({"job_id": job_id, "status": "processing"})
     except Job.DoesNotExist:
